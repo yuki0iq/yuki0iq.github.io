0b8e978276b347c8237251b26d645cd272e68725
長野ユキ <44705138+yuki0iq@users.noreply.github.com>
2022-05-05 17:16:19 +0300

    Create test.js

---
+++ test.js
@@ -0,0 +1,15 @@
+function create(data) {
+  // data: Array of [Name, Questions]
+  // Questions: Array of [where, Name]
+  for (const entry of data) {
+    gn = entry[0]
+    // add H4
+    questions = entry[1]
+    // begin table
+    for (const question of questions) {
+      tx = question[1]
+      // add row
+    }
+    // end table
+  }
+}
