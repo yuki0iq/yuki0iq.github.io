4f5449fe0d15b2707e4e60a16cedbd5b9fd897f1
長野ユキ <44705138+yuki0iq@users.noreply.github.com>
2022-05-05 22:57:57 +0300

    Update ennea.js

--- ennea.js
+++ ennea.js
@@ -278,8 +278,8 @@ function recalc() {
   
   tritypew = []
   for (i of [[E2w1, '2w1'], [E2w3, '2w3'], [E3w2, '3w2'], [E3w4, '3w4'], [E4w3, '4w3'], [E4w5, '4w5']]) {
-    for (j of [5, 6, 7]) {
-      for (k of [8, 9, 1]) {
+    for (j of [[E5w4, '5w4'], [E5w6, '5w6'], [E6w5, '6w5'], [E6w7, '6w7'], [E7w6, '7w6'], [E7w8, '7w8']]) {
+      for (k of [[E8w7, '8w7'], [E8w9, '8w9'], [E9w1, '9w1'], [E9w8, '9w8'], [E1w2, '1w2'], [E1w9, '1w9']]) {
         tritypew.push([5*vans[i[0]] + 3*vans[j[0]] + 1*vans[k[0]], i[1] + j[1] + k[1]])  //ijk
         tritypew.push([5*vans[i[0]] + 3*vans[k[0]] + 1*vans[j[0]], i[1] + k[1] + j[1]])  //ikj
         tritypew.push([5*vans[j[0]] + 3*vans[i[0]] + 1*vans[k[0]], j[1] + i[1] + k[1]])  //jik
