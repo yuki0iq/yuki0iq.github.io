f95c482d6d2b6c5f931aa6120d4a774d877a8e1d
長野ユキ <44705138+yuki0iq@users.noreply.github.com>
2022-05-05 19:02:45 +0300

    Create mbti.js

---
+++ mbti.js
@@ -0,0 +1,14 @@
+question_sets = [
+  ['Temp', 'This is a rofl', [['Ni',0],['Ne',1],['Si',2],['Se',3],['Fi',4],['Fe',5],['Ti',6],['Te',7]]],
+]
+
+create(question_sets)
+console.log(question_sets)
+
+// temp
+function recalc() {
+  alert("yes.");
+}
+
+
+recalc();  // Initial recalc
