94573df1699b9ee0c8e60169a807b90f54b5922f
長野ユキ <44705138+yuki0iq@users.noreply.github.com>
2022-05-05 17:52:48 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,15 +1,49 @@
 function create(data) {
   // data: Array of [Name, Questions]
   // Questions: Array of [where, Name]
+  str = ''
+  i = 0
   for (const entry of data) {
     gn = entry[0]
     // add H4
     questions = entry[1]
+    str += '<h4>' + questions + '</h4>'
     // begin table
+    str += '<table><tr>'
+    str += '<th>No</th>'
+    str += '<th>Вопрос</th>'
+    str += '<th>Ответ</th>'
+    str += '</tr>'
+    j = 0
     for (const question of questions) {
       tx = question[1]
       // add row
+      str += '<tr>'
+      str += '<td>' + j + '</td>'
+      str += '<td>' + tx + '</td>'
+      str += '<td><form id="form' + i + '-' + j + '">'
+      qqq = 'i' + i + '-' + j + 'a'
+      str += '<input id="' + qqq + '" value=2 type="radio"> <label for="' + qqq + '">Да</label>'
+      qqq = 'i' + i + '-' + j + 'b'
+      str += '<input id="' + qqq + '" value=1 type="radio"> <label for="' + qqq + '"></label>'
+      qqq = 'i' + i + '-' + j + 'c'
+      str += '<input id="' + qqq + '" value=0 type="radio"> <label for="' + qqq + '"></label>'
+      qqq = 'i' + i + '-' + j + 'd'
+      str += '<input id="' + qqq + '" value=-1 type="radio"> <label for="' + qqq + '">Нет</label>'
+      str += '</form></td>'
+      str += '</tr>'
+      j = j - (-1)
     }
     // end table
+    str += '</table>'
+    i = i - (-1)
   }
+  document.getElementById('test_contents').innerHTML = str;
 }
+
+create(
+  [
+    ["Test1", [ ['q1', 1], ['q2', 1], ['q3', 1] ] ],
+    ["Test2", [ ['qq', 1], ['qw', 1], ['qe', 1] ] ]
+  ]
+);
