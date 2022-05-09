0799e25c8e7127888e0d1366db348dc120e9ab4e
Yuki <example@example.com>
2022-05-09 19:57:04 +0300

    kek

--- mbti2.js
+++ mbti2.js
@@ -1,3 +1,4 @@
+// Cognitive functions
 aNi = ['Ni', 2, 1, 0, -1]
 aNe = ['Ne', 2, 1, 0, -1]
 aSi = ['Si', 2, 1, 0, -1]
@@ -6,6 +7,58 @@ aFi = ['Fi', 2, 1, 0, -1]
 aFe = ['Fe', 2, 1, 0, -1]
 aTi = ['Ti', 2, 1, 0, -1]
 aTe = ['Te', 2, 1, 0, -1]
+// Preference
+aNiSeT = ['NiSe', 3, 2, 1, 0, -1]
+aNeSiT = ['NeSi', 3, 2, 1, 0, -1]
+aFiTeT = ['FiTe', 3, 2, 1, 0, -1]
+aFeTiT = ['FeTi', 3, 2, 1, 0, -1]
+aNiSeF = ['NiSe', -1, 0, 1, 2, 3]
+aNeSiF = ['NeSi', -1, 0, 1, 2, 3]
+aFiTeF = ['FiTe', -1, 0, 1, 2, 3]
+aFeTiF = ['FeTi', -1, 0, 1, 2, 3]
+// Secondary preference
+aNiP = ['NiP', 2, 1, 0, -1]
+aNeP = ['NeP', 2, 1, 0, -1]
+aSiP = ['SiP', 2, 1, 0, -1]
+aSeP = ['SeP', 2, 1, 0, -1]
+aFiP = ['FiP', 2, 1, 0, -1]
+aFeP = ['FeP', 2, 1, 0, -1]
+aTiP = ['TiP', 2, 1, 0, -1]
+aTeP = ['TeP', 2, 1, 0, -1]
+// loops
+alNiFe = ['lNiFe', 2, 1, 0, -1]
+alNiTe = ['lNiTe', 2, 1, 0, -1]
+alNeFi = ['lNeFi', 2, 1, 0, -1]
+alNeTi = ['lNeTi', 2, 1, 0, -1]
+alSiFe = ['lSiFe', 2, 1, 0, -1]
+alSiTe = ['lSiTe', 2, 1, 0, -1]
+alSeFi = ['lSeFi', 2, 1, 0, -1]
+alSeTi = ['lSeTi', 2, 1, 0, -1]
+alFiNe = ['lFiNe', 2, 1, 0, -1]
+alFiSe = ['lFiSe', 2, 1, 0, -1]
+alFeNi = ['lFeNi', 2, 1, 0, -1]
+alFeSi = ['lFeSi', 2, 1, 0, -1]
+alTiNe = ['lTiNe', 2, 1, 0, -1]
+alTiSe = ['lTiSe', 2, 1, 0, -1]
+alTeNi = ['lTeNi', 2, 1, 0, -1]
+alTeSi = ['lTeSi', 2, 1, 0, -1]
+// grips
+agNiFe = ['gNiFe', 2, 1, 0, -1]
+agNiTe = ['gNiTe', 2, 1, 0, -1]
+agNeFi = ['gNeFi', 2, 1, 0, -1]
+agNeTi = ['gNeTi', 2, 1, 0, -1]
+agSiFe = ['gSiFe', 2, 1, 0, -1]
+agSiTe = ['gSiTe', 2, 1, 0, -1]
+agSeFi = ['gSeFi', 2, 1, 0, -1]
+agSeTi = ['gSeTi', 2, 1, 0, -1]
+agFiNe = ['gFiNe', 2, 1, 0, -1]
+agFiSe = ['gFiSe', 2, 1, 0, -1]
+agFeNi = ['gFeNi', 2, 1, 0, -1]
+agFeSi = ['gFeSi', 2, 1, 0, -1]
+agTiNe = ['gTiNe', 2, 1, 0, -1]
+agTiSe = ['gTiSe', 2, 1, 0, -1]
+agTeNi = ['gTeNi', 2, 1, 0, -1]
+agTeSi = ['gTeSi', 2, 1, 0, -1]
 
 question_sets = [
   ['Ne против Ni', '', 4, [
@@ -84,15 +137,27 @@ function recalc() {
 
   out = function(val, mx) {
     return '<td>' + val.toString() + (val == mx ? '!' : '') + '</td>'
-  }  
+  }
+  ou = function(val) {
+    return '<td>' + val + '</td>'
+  }
 
   str += '<h3>Исходный результат</h3>'
-  str += '<table><tr>'
-  str += '<th>Ni</th><th>Ne</th><th>Si</th><th>Se</th><th>Fi</th><th>Fe</th><th>Ti</th><th>Te</th><th>Группа</th></tr>'
-  // get vans
-  gNi = 0, gNe = 0, gSi = 0, gSe = 0, gFi = 0, gFe = 0, gTi = 0, gTe = 0
+
+  Ni = 0, Ne = 0, Si = 0, Se = 0, Fi = 0, Fe = 0, Ti = 0, Te = 0
+  NiSe = 0, NeSi = 0, FiTe = 0, FeTi = 0
+  NiP = 0, NeP = 0, SiP = 0, SeP = 0, FiP = 0, FeP = 0, TiP = 0, TeP = 0
+  lNiFe = 0, lNiTe = 0, lNeFi = 0, lNeTi = 0
+  lSiFe = 0, lSiTe = 0, lSeFi = 0, lSeTi = 0
+  lFiNe = 0, lFiSe = 0, lFeNi = 0, lFeSi = 0
+  lTiNe = 0, lTiSe = 0, lTeNi = 0, lTeSi = 0
+  gNiFe = 0, gNiTe = 0, gNeFi = 0, gNeTi = 0
+  gSiFe = 0, gSiTe = 0, gSeFi = 0, gSeTi = 0
+  gFiNe = 0, gFiSe = 0, gFeNi = 0, gFeSi = 0
+  gTiNe = 0, gTiSe = 0, gTeNi = 0, gTeSi = 0
+
+  // <==== Rebuild table
   for (i = 0; i < question_sets.length; i += 1) {
-    Ni = 0, Ne = 0, Si = 0, Se = 0, Fi = 0, Fe = 0, Ti = 0, Te = 0
     for (j = 0; j < question_sets[i][3].length; j += 1) {
       i_n = 'i' + i + '-' + j
       v_sub = $('input[name=' + i_n + ']:checked')
@@ -101,35 +166,38 @@ function recalc() {
         console.log(v_sub[0].value)
       }
     }
-    gNi += Ni, gNe += Ne, gSi += Si, gSe += Se, gFi += Fi, gFe += Fe, gTi += Ti, gTe += Te
-    mx = Math.max(Ni, Ne, Si, Se, Fi, Fe, Ti, Te)
-    str += '<tr>'
-    str += out(Ni, mx)
-    str += out(Ne, mx)
-    str += out(Si, mx)
-    str += out(Se, mx)
-    str += out(Fi, mx)
-    str += out(Fe, mx)
-    str += out(Ti, mx)
-    str += out(Te, mx)
-    str += '<td>' + question_sets[i][0] + '</td>'
-    str += '</tr>'
   }
-  Ni = gNi, Ne = gNe, Si = gSi, Se = gSe, Fi = gFi, Fe = gFe, Ti = gTi, Te = gTe
-  mx = Math.max(Ni, Ne, Si, Se, Fi, Fe, Ti, Te)
-  str += '<tr>'
-  str += out(Ni, mx)
-  str += out(Ne, mx)
-  str += out(Si, mx)
-  str += out(Se, mx)
-  str += out(Fi, mx)
-  str += out(Fe, mx)
-  str += out(Ti, mx)
-  str += out(Te, mx)
-  str += '<td>ИТОГ</td>'
-  str += '</tr>'
+  str += 'Когнитивные функции'
+  str += '<table>'
+  str += '<tr> <th>Ni</th> <th>Ne</th> <th>Si</th> <th>Se</th> <th>Fi</th> <th>Fe</th> <th>Ti</th> <th>Te</th> <th>Группа</th> </tr>'
+  str += '<tr>' + ou(Ni)  + ou(Ne)  + ou(Si)  + ou(Se)  + ou(Fi)  + ou(Fe)  + ou(Ti)  + ou(Te)  + ou('Ne vs Ni; Se vs Si; Fe vs Fi; Te vs Ti') + '</tr>'
+  str += '<tr>' + ou(NiP) + ou(NeP) + ou(SiP) + ou(SeP) + ou(FiP) + ou(FeP) + ou(TiP) + ou(TeP) + ou('Превосходство по осям') + '</tr>'
   str += '</table>'
-  str += 'Возможный разброс баллов от -7 до 14'
+  str += 'Когнитивные оси'
+  str += '<table>'
+  str += '<tr> <th>NiSe</th> <th>NeSi</th> <th>FiTe</th> <th>FeTi</th>'
+  str += '<tr>' + ou(NiSe) + ou(NeSi) + ou(FiTe) + ou(FeTi) + '</tr>'
+  str += '</table>'
+  str += 'Лупы и грипы'
+  str += '<table>'
+  str += '<tr> <th>Тип</th> <th>Луп</th> <th>Грип</th> </tr>'
+  str += '<tr>' + ou('NiFe') + ou(lNiFe) + ou(gNiFe) + '</tr>'
+  str += '<tr>' + ou('NiTe') + ou(lNiTe) + ou(gNiTe) + '</tr>'
+  str += '<tr>' + ou('NeFi') + ou(lNeFi) + ou(gNeFi) + '</tr>'
+  str += '<tr>' + ou('NeTi') + ou(lNeTi) + ou(gNeTi) + '</tr>'
+  str += '<tr>' + ou('SiFe') + ou(lSiFe) + ou(gSiFe) + '</tr>'
+  str += '<tr>' + ou('SiTe') + ou(lSiTe) + ou(gSiTe) + '</tr>'
+  str += '<tr>' + ou('SeFi') + ou(lSeFi) + ou(gSeFi) + '</tr>'
+  str += '<tr>' + ou('SeTi') + ou(lSeTi) + ou(gSeTi) + '</tr>'
+  str += '<tr>' + ou('FiNe') + ou(lFiNe) + ou(gFiNe) + '</tr>'
+  str += '<tr>' + ou('FiSe') + ou(lFiSe) + ou(gFiSe) + '</tr>'
+  str += '<tr>' + ou('FeNi') + ou(lFeNi) + ou(gFeNi) + '</tr>'
+  str += '<tr>' + ou('FeSi') + ou(lFeSi) + ou(gFeSi) + '</tr>'
+  str += '<tr>' + ou('TiNe') + ou(lTiNe) + ou(gTiNe) + '</tr>'
+  str += '<tr>' + ou('TiSe') + ou(lTiSe) + ou(gTiSe) + '</tr>'
+  str += '<tr>' + ou('TeNi') + ou(lTeNi) + ou(gTeNi) + '</tr>'
+  str += '<tr>' + ou('TeSi') + ou(lTeSi) + ou(gTeSi) + '</tr>'
+  str += '</table>
 
   names = new Map()
   names.set('INTJ', 'Стратег')
@@ -171,7 +239,7 @@ function recalc() {
   str += '</table>'
   str += 'Наиболее вероятный тип по буквам: <b>' + str2 + '</b> (' + names.get(str2) + ')'
 
-  // 7a + 5b + 3c + 1d
+  // 7dom + 5aux + 3ter + 1inf + 7dominf + 5auxter + 6dom>inf + 4aux>ter + 5loop + 5grip
   mbti = [
     [7*Ni + 5*Fe + 3*Ti + 1*Se, 'INFJ'],
     [7*Ni + 5*Te + 3*Fi + 1*Se, 'INTJ'],
@@ -191,14 +259,14 @@ function recalc() {
     [5*Ti + 7*Se + 1*Ni + 3*Fe, 'ESTP'],
   ]
   mbti.sort(function(a,b) { if (a[0] - b[0] != 0) return a[0] - b[0]; return a[1] < b[1];})
-  str += '<h3>Тип по когнитивкам</h3> Формула mistypeinvestigator. Самый вероятный тип имеет наибольшее число (как на сакинорве)'
+  str += '<h3>Тип по когнитивкам</h3> Модифицированная формула mistypeinvestigator. Самый вероятный тип имеет наибольшее число (как на сакинорве)'
   str += '<table>'
   str += '<tr><th>Тип</th><th>Счёт</th>'
   for (i = 0; i < 16; i += 1) {
     str += '<tr><td>' + mbti[i][1] + '</td><td>' + mbti[i][0] + '</td></tr>'
   }
   str += '</table>'
-  str += 'Наиболее вероятный тип по когнитивным функциям: <b>' + mbti[15][1] + '</b>. Возможный разброс баллов от -112 до 224'
+  str += 'Наиболее вероятный тип по когнитивным функциям: <b>' + mbti[15][1] + '</b>. Возможный разброс баллов от XXX до XXX'
   
   document.getElementById('res').innerHTML = str
 }
