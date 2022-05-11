6ddcedac2ca21a64bc76f26843ed8843bce5d451
Yuki <example@example.com>
2022-05-11 16:00:21 +0300

    kek

--- 16p.js
+++ 16p.js
@@ -1,61 +1,31 @@
 question_sets = [
-  ['Тест Агасфера', '', 1, [
-    [4, 'Ты говоришь'],
-    [1, 'Ты умеешь думать'],
-    [6, 'Ты убил человека'],
-    [3, 'Ты моешься'],
+  ['Тест Агасфера', '', 5, 1, [
+    ['Ты говоришь', [['E', 2, 1, 0, -1, -2], ['I', -2, -1, 0, 1, 2]]],
+    ['Ты умеешь думать', [['N', 2, 1, 0, -1, -2], ['S', -2, -1, 0, 1, 2]]],
+    ['Ты убил человека', [['T', 2, 1, 0, -1, -2], ['F', -2, -1, 0, 1, 2]]],
+    ['Ты моешься', [['J', 2, 1, 0, -1, -2], ['P', -2, -1, 0, 1, 2]]]
     ]],
 ]
 
 for (i = 0; i < question_sets.length; i += 1) {
-  shuffle(question_sets[i][3])
+  shuffle(question_sets[i][4])
 }
 create(question_sets)
 
 function recalc() {
   str = ''
   
-  // get vans
-  vans = [0, 0, 0, 0]
-  q = 0
+  E = 0, I = 0, N = 0, S = 0, T = 0, F = 0, J = 0, P = 0
   for (i = 0; i < question_sets.length; i += 1) {
-    for (j = 0; j < question_sets[i][3].length; j += 1) {
+    for (j = 0; j < question_sets[i][4].length; j += 1) {
       i_n = 'i' + i + '-' + j
-      f_n = '#form' + i + '-' + j
-      v_sub = $('input[name=' + i_n + ']:checked', f_n)
-      if (v_sub.length == 0) {  // Not selected
-        v_sub = 0
-      } else {
-        v_sub = -v_sub[0].value
+      v_sub = $('input[name=' + i_n + ']:checked')
+      if (v_sub.length != 0) {  // selected
+        eval(v_sub[0].value)
+        console.log(v_sub[0].value)
       }
-      wh = question_sets[i][3][j][0]
-      console.log(wh, v_sub)
-      if (wh == 4 || wh == 5 || wh == 6 || wh == 7) {
-        wh -= 4
-        v_sub = -v_sub
-      }
-      vans[wh] -= v_sub  // yeah, the shit..
-      q -= v_sub * v_sub
     }
   }
-  str += '[I>E] [N>S] [F>T] [J>P]\n'
-  mx = 0
-  for (j = 0; j < 4; j += 1) {
-    if (vans[j] > mx) {
-      mx = vans[j]
-    }
-  }
-  for (j = 0; j < 4; j += 1) {
-    str += rjust(vans[j].toString(), 4)
-    if (vans[j] == mx) {
-      str += '!'
-    } else {
-      str += ' '
-    }
-    str += ' '
-  }
-  str += '\n'
-  str += '\n'
   
   names = new Map()
   names.set('INTJ', 'Strateg')
@@ -75,20 +45,17 @@ function recalc() {
   names.set('ESTP', 'Delec')
   names.set('ESFP', 'Razvlekatel')
 
-  str += 'Твой тип: '
   str2 = ''
-  if (vans[0] > 0) { str2 += 'I' } else { str2 += 'E' }
-  if (vans[1] > 0) { str2 += 'N' } else { str2 += 'S' }
-  if (vans[2] > 0) { str2 += 'F' } else { str2 += 'T' }
-  if (vans[3] > 0) { str2 += 'J' } else { str2 += 'P' }
-  str += str2
-  str += '-'
-  if (-q == 16) { str += 'A' } else { str += 'T' }
-  str += ' '
-  str += names.get(str2)
-  str += '\n'
+  if (I - E > 0) { str2 += 'I' } else { str2 += 'E' }
+  if (N - S > 0) { str2 += 'N' } else { str2 += 'S' }
+  if (F - T > 0) { str2 += 'F' } else { str2 += 'T' }
+  if (J - P > 0) { str2 += 'J' } else { str2 += 'P' }
+
+  str += 'Твой тип: <b>' + str2 + '-'
+  if (E * E + N * N + F * F + P * P == 8) { str += 'A' } else { str += 'T' }
+  str += '</b> ' + names.get(str2)
   
-  document.getElementById('res').innerHTML = '<pre>' + str + '</pre>'
+  document.getElementById('res').innerHTML = str
 }
 
 recalc();  // Initial recalc
