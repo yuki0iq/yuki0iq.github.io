8db565340555d546a62e54a143ca4e8cd01cf9ce
Yuki <example@example.com>
2022-08-18 22:30:14 +0300

    kek

--- google-sheets-updater.js
+++ google-sheets-updater.js
@@ -1,30 +1,6 @@
-const API_KEY = 'AIzaSyAJSLa7fH8JM715HVmhTRCPvnBNl9YaXrs';
-const DISCOVERY_DOC = 'https://sheets.googleapis.com/$discovery/rest?version=v4';
-const CLIENT_ID = '117134655696470126940';
-const SCOPE = 'https://www.googleapis.com/auth/spreadsheets';
-
-let gapiInited = false;
-
-function gapiLoaded() {
-	gapi.load('client', intializeGapiClient);
-}
-
-function intializeGapiClient() {
-	gapi.client.init({
-		apiKey: API_KEY,
-		discoveryDocs: [DISCOVERY_DOC],
-		clientId: CLIENT_ID,
-		scope: SCOPE,
-	}).then(function () {
-		gapiInited = true;
-		console.log('google api init ok');
-	});
-}
-
-
 function get_cell_names() {
 	const offset_rows = 2;
-	const cols = ['B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+	const offset_cols = 2;
 
 	let cells = [];  // list of cells to increment; one for each answered question
 
@@ -37,10 +13,10 @@ function get_cell_names() {
 				eval(v_sub[0].value);
 				let k = parseInt(v_sub[0].id.substr(1 + v_sub[0].name.length));
 				let question = new Map(questions[j]);
-				let _id = question.get('_id') + offset_rows;
+				let _id = question.get('_id');
 				let _rev = question.get('_rev') || 0;
 				if (_rev) k = question_set.get('cnt_q') - 1 - k;
-				cell = 'Sheet1!' + cols[k] + _id;
+				cell = [_id + offset_rows, k + offset_cols];
 				cells.push(cell);
 			}
 		}
